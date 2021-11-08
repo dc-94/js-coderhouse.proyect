42f4933811eb9b58d6b314dedc86ca0061a35a32
Dan Coronel <dan.c94@outlook.com>
2021-11-08 10:23:27 -0300

    4to desafio

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let sum = 0;
-let terminar = true;
 let cant = 0;
+let item = parseInt(prompt("ingresa el precio del primer producto"))
+
+
+
+/*
+let terminar = true;
 while(terminar != "no") {
     // let item = prompt("Ingrese un item").toUpperCase()
     let precio = parseInt(prompt("Ingrese el precio"))
@@ -10,12 +15,18 @@ while(terminar != "no") {
 }
 document.write(`<p>El total es: <strong>$${sum}</strong></p>`)
 let dividir = prompt("¿Desea dividir la cuenta? (SI / NO").toLowerCase()
-// alert("El total es:" + /*item + " " +*/ " " + "$"+sum )
+// alert("El total es:" +  " " + "$"+sum )
 if (dividir == 'si') {
     let personas = parseInt(prompt("Ingrese cantidad de comensales"))
     let parte = (sum / personas)
     document.write(`<p class="sub">A cada persona le corresponde: <strong>$${parte}</strong></p>`)
-}
+}*/
+
+
+
+
+
+
 // alert("El total es:" + " " + "$"+sum + " " +
 //     "A cada persona le corresponde" + " " + "$"+parte)
 
