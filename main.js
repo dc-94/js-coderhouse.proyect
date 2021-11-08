8373cb345cc3614f6f7b8820cf82b6dc967229b0
Dan Coronel <dan.c94@outlook.com>
2021-11-08 14:56:56 -0300

    ee

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let sum = 0;
 let cant = 0;
 let item = parseInt(prompt("ingresa el precio del primer producto"))
@@ -27,6 +28,33 @@ if (dividir == 'si') {
 
 
 
+=======
+
+
+
+
+
+
+
+// let sum = 0;
+// let terminar = true;
+// let cant = 0;
+// while(terminar != "no") {
+//     // let item = prompt("Ingrese un item").toUpperCase()
+//     let precio = parseInt(prompt("Ingrese el precio"))
+    
+//     sum += precio
+//     terminar = prompt("¿Desea agregar mas items? ( SI / NO )").toLowerCase();
+// }
+// document.write(`<p>El total es: <strong>$${sum}</strong></p>`)
+// let dividir = prompt("¿Desea dividir la cuenta? (SI / NO").toLowerCase()
+// // alert("El total es:" + /*item + " " +*/ " " + "$"+sum )
+// if (dividir == 'si') {
+//     let personas = parseInt(prompt("Ingrese cantidad de comensales"))
+//     let parte = (sum / personas)
+//     document.write(`<p class="sub">A cada persona le corresponde: <strong>$${parte}</strong></p>`)
+// }
+>>>>>>> Stashed changes
 // alert("El total es:" + " " + "$"+sum + " " +
 //     "A cada persona le corresponde" + " " + "$"+parte)
 
