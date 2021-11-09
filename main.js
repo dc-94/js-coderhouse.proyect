827568dbfd742c2a06c4151f2d56c2dad2517537
Dan Coronel <dan.c94@outlook.com>
2021-11-09 17:58:32 -0300

    4to desafio intento3-fnciona

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 
 let IVA = 1.21
-
-     const total = (precio1,precio2) =>  precio1 + precio2
-    const precio1 = () => parseInt(prompt("Ingrese un precio de producto"))
-    const precio2 = () => parseInt(prompt("Ingrese un precio de producto"))
-     //terminar = () => prompt("¿Desea agregar mas items? ( SI / NO )").toLowerCase();
+const total = (precio1,precio2) =>  precio1 + precio2
+const precio1 = () => parseInt(prompt("Ingrese un precio de producto"))
+const precio2 = () => parseInt(prompt("Ingrese un precio de producto"))
 const resultadoNeto = (total) => console.log(`El total Neto es ${total}`)
-    const final = (total) => total * IVA
+const final = (total) => total * IVA
 const resultadoConIVA = (final) => console.log(`El total con iva es ${final}`)
 
 resultadoNeto(total(precio1(),precio2()))
