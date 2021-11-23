815d99ce4bc3e230e885493136d4e7086e4b1482
Dan Coronel <dan.c94@outlook.com>
2021-11-23 13:34:19 -0300

    desafio 8

--- main.js
+++ main.js
@@ -1,147 +1,136 @@
-class Note{
-    constructor(title, content, priority){
-        this.title = title
-        this.content = content
-        this.priority = priority
-    }
-    showNotes(){
-        return `
-        - ${this.title} 
-        - ${this.content} 
-        - ${this.priority}`
-    }
-}
+let bodega = []
 
-const nota1 = new Note("primera nota","Espero funcione", 4)
-const nota2 = new Note("second one","damn, I hate deadlines", 2)
-const nota3 = new Note(prompt("Ingrese titulo"), prompt("Ingrese texto"), parseInt(prompt("Ingrese prioridad: (1 a 5)")))
-function localSave(){
-    let toJSON = JSON.stringify(Note)
-    localStorage.setItem("note", toJSON)
-}
-localSave()
-/*
-class Auto{ 
-    constructor(marca, modelo, puertas, precio){
-    this.marca = marca ;
-    this.modelo= modelo ;
-    this.puertas = puertas ;
-    this.precio = precio ;
-    }
-    aptoFlia(){
-        if((this.puertas) >= 5 ){
-            if(this.precio <= 3000000){
-            console.log(`${this.marca} ${this.modelo} es apto para tu familia`)
-            }else{
-                console.log(`${this.marca} ${this.modelo} es apto para tu familia...pero reee caro`)
-            }
-        }else{
-            if(this.precio <= 3000000){
-                console.log(`${this.marca} ${this.modelo} serviría si aún fueras soltero`)
-            }else{
-            console.log(`${this.marca} ${this.modelo} serviría si aún fueras soltero y tuvieras $$$`)
-
-            }
-        }
+class Vino {
+    constructor(nombre, cepa, bodega,img, cosecha, precio){
+    this.nombre = nombre
+    this.cepa = cepa
+    this.bodega = bodega
+    this.img = img
+    this.cosecha = cosecha
+    this.precio = precio
     }
 }
-const auto1 = new Auto("Volkswagen", "Suran", 5, 1090000)
-const auto2 = new Auto("BMW", "X5", 5, 11980000)
-const auto3 = new Auto("Smart", "Fortwo", 2, 2000000)
-const auto4 = new Auto("Porsche", "911", 3, 25980000)
-const auto5 = new Auto(prompt("Marca del auto:"),prompt("Modelo:"), parseInt(prompt("Cantidad de puertas:")), parseInt(prompt("Precio:")))
-auto1.aptoFlia()
-auto2.aptoFlia()
-auto3.aptoFlia()
-auto4.aptoFlia()
-auto5.aptoFlia()
 
-let sum = 0;
-let terminar = true;
-let cant = 0;
-while(terminar != "no") {
-    // let item = prompt("Ingrese un item").toUpperCase()
-    let precio = parseInt(prompt("Ingrese el precio"))
-    
-    sum += precio
-    terminar = prompt("¿Desea agregar mas items? ( SI / NO )").toLowerCase();
-}
-document.write(`<p>El total es: <strong>$${sum}</strong></p>`)
-let dividir = prompt("¿Desea dividir la cuenta? (SI / NO").toLowerCase()
-
-if (dividir == 'si') {
-    let personas = parseInt(prompt("Ingrese cantidad de comensales"))
-    let parte = (sum / personas)
-    document.write(`<p class="sub">A cada persona le corresponde: <strong>$${parte}</strong></p>`)
-} 
+bodega.push(new Vino("Carpe Diem", "Pinot Noir","La Espera","https://mly1ygebnr2t.i.optimole.com/ScIDir0.w5KU~57308/w:600/h:600/q:89/https://vinotecaleneas.com/wp-content/uploads/2020/11/La-Espera-Carpe-Diem-Pinot-Noir.jpg",2018, 780))
+bodega.push(new Vino("Mas Janeil Le Petit Pas", "Grenache Noir / Syrah / Carignan / Mourvedre", "Francois Lurton","https://images.vivino.com/thumbs/P6vDe0gcTJCVrTwGofbMzg_pb_600x600.png", 2016, 2120))
+bodega.push(new Vino("Monteagrelo", "Cabernet Franc", "Bressia","https://images.vivino.com/thumbs/4Ug9g_psSOejUXKDVSt4TA_pl_375x500.png", 2019, 2200))
+bodega.push(new Vino("Humberto Canale Estate", "Cabernet Franc", "Humberto Canale","https://vinotecaligier.com/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/b/e/be03905_2.jpg", 2019, 900))
+bodega.push(new Vino("Golden Reserve", "Malbec", "Trivento","http://cdn.shopify.com/s/files/1/0215/7051/9140/products/trivento-golden-reserve-malbec-2019-vino-trivento-438468.jpg?v=1629756944", 2019, 1250))
+bodega.push(new Vino("Corte Argentino", "Malbec", "Francois Lurton","https://www.espaciovino.com.ar/media/default/0001/53/thumb_52818_default_medium.jpeg", 2013, 3470))
+bodega.push(new Vino("Cono Sur Reserva", "Carmenere", "Cono Sur","https://vinotecaligier.com/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/v/i/vi10924_1.jpg", 2018, 1450))
+bodega.push(new Vino("Trivento Reserva", "Cabernet Sauvignon", "Trivento","https://www.espaciovino.com.ar/media/default/0001/58/thumb_57329_default_medium.jpeg", 2020, 490))
 
 
-// alert("El total es:" + " " + "$"+sum + " " +
-//     "A cada persona le corresponde" + " " + "$"+parte)
-
-
-/*let nombre = (prompt("¿Cual es tu Nombre y Apellido?"))
-let nacimiento = parseFloat(prompt("¿En que año naciste?"))
-let edad = (2021 - nacimiento)
-let pais = prompt("País de origen").toLocaleLowerCase()
-
-
-if (edad >= 18) {
-        let edad = 'may'
-    if (pais == "argentina"){
-        let nacionalidad = 'arg'
-        // console.log("mayor y arg")
-        document.write(`<div class="carnet">
-        <img src="cat.webp" alt="" >
-        <div class="txt">
-            <h1>${nombre}</h1>
-            <br>
-            <h3>Año de Nacimiento:</h3>
-            <h2 class="${edad}">${nacimiento}</h2>
-            <h2 class="${nacionalidad}">${pais}</h2>
+let mainCards = document.getElementById("mainCards")
+bodega.forEach(Vino => {
+    mainCards.innerHTML += `
+    <div class="mainCards_card">
+        <img class="mainCards_card_img" src="${Vino.img}" alt="">
+        <div class="mainCards_card_txt">
+            <h2 class="mainCards_card_txt_productName">${Vino.nombre}</h2>
+            <h4 class="mainCards_card_txt_desc">${Vino.bodega}</h4>
+            <h3 class="mainCards_card_txt_cepa">${Vino.cepa}
+            <br>- ${Vino.cosecha}
+            <p class="mainCards_card_txt_price">$${Vino.precio}</p>
         </div>
-        </div>`)                
-    }
-    else{
-        // console.log("mayor y extr")
-        let nacionalidad = 'ext'
-        document.write(`<div class="carnet">
-        <img src="cat.webp" alt="" >
-        <div class="txt">
-            <h1>${nombre}</h1>
-            <br>
-            <h3>Año de Nacimiento:</h3>
-            <h2 class="${edad}">${nacimiento}</h2>
-            <h2 class="${nacionalidad}">${pais}</h2>
-        </div>
-        </div>`)
-    }
-}else if (pais == "argentina"){
-    let nacionalidad = 'arg'
-    // console.log("menor y arg")
-    document.write(`<div class="carnet">
-    <img src="cat.webp" alt="" >
-    <div class="txt">
-        <h1>${nombre}</h1>
-        <br>
-        <h3>Año de Nacimiento:</h3>
-        <h2 class="${edad}">${nacimiento}</h2>
-        <h2 class="${nacionalidad}">${pais}</h2>
     </div>
-    </div>`)
-}else{
-    let edad = "men"
-    let nacionalidad = 'ext'
-    // console.log("menor y extr")
-    document.write(`<div class="carnet">
-        <img src="cat.webp" alt="" >
-        <div class="txt">
-            <h1>${nombre}</h1>
-            <br>
-            <h3>Año de Nacimiento:</h3>
-            <h2 class="${edad}">${nacimiento}</h2>
-            <h2 class="${nacionalidad}">${pais}</h2>
-        </div>
-    </div>`)    
+    `
+})
+//let addItem = document.getElementById("addItem")
+
+
+// guardar JSON
+/*function localSave(){
+    let aJson = JSON.stringify(bodega)
+    localStorage.setItem("bodega",aJson)
 }
-*/
+localSave()*/
+//leer JSON
+// function localRead(){
+//     let deJson= localStorage.getItem("bodega")
+//     const bodega = JSON.parse(deJson)
+//}
+//
+// //pregunta si agregar mas o no
+function masVino(){
+    let morewine = prompt("desea agregar mas vino? SI/NO").toLowerCase()
+    if(morewine == "si"){
+        agregarVino()
+        masVino()
+    }else if(morewine == "no"){
+        mainQuestion()
+    }else{
+     alert("Era SI o NO lo que tenias que responder!")   
+    }
+}
+//
+//agregar vino
+function agregarVino(){
+    bodega.push(new Vino(prompt("Ingrese el nambre del vino"), prompt("Ingrese la cepa"), 
+    prompt("Ingrese la bodega"), 
+    parseInt(prompt("Ingrese el año de cosecha")), 
+    parseInt(prompt("Ingrese el precio"))
+        )
+    )
+//    localSave()
+    masVino()
+}
+// masVino()
+// //
+// //orden alfabetico
+// function ordenABC(){
+//     bodega.sort(function(a,b){
+//         return a.nombre > b.nombre
+//     })
+//     console.log(bodega)
+// }
+// //
+// //por precio > <
+// function searchPrecio(){
+//     let valor = parseInt(prompt("ingrese un precio (490 a 3580)"))
+//     let orden = prompt("desea buscar productos por debajo o sobre ese precio? ( < ó > )")
+//     if (orden == "<"){
+//         console.log(bodega.filter(vino => vino.precio <= valor))
+//     }else if(orden == ">"){
+//         console.log(bodega.filter(vino => vino.precio >= valor))
+//     }else{
+//         console.log("ingresaste un carácter no valido")
+//     }
+// }
+// //
+// //por cosecha
+// function searchCosecha(){
+//     let valor = parseInt(prompt("ingrese un año (2013 a 2020)"))
+//     let orden = prompt("desea buscar productos por debajo o sobre ese año de cosecha? ( < ó > )")
+//     if (orden == "<"){
+//         console.log(bodega.filter(vino => vino.cosecha <= valor))
+//     }else if(orden == ">"){
+//         console.log(bodega.filter(vino => vino.cosecha >= valor))
+//     }else{
+//         console.log("ingresaste un carácter no valido")
+//     }
+// }
+// //
+// //como buscar?
+// function searchMethod(){
+//     let searchQ = prompt(" desea ordenar los productos por orden alfabetico (A) ó buscar por precio (P) o cosecha (C)?").toUpperCase()
+//     if (searchQ == "A"){
+//         ordenABC()
+//     }else if(searchQ == "P"){
+//         searchPrecio()
+//     }else{
+//         searchCosecha()
+//     }
+// }
+// //que desea hacer?
+// function mainQuestion(){
+//     let firstQ = prompt("Quiere agregar, buscar un vino ó consultar el catálogo? (A/B/C)").toUpperCase()
+//     if (firstQ == "A"){
+//         agregarVino()
+//     }else if(firstQ == "B"){
+//         searchMethod()
+//     }else{
+//         alert("Podés consultar el catálogo en la consola y local Storage")
+//     }
+// }
+// mainQuestion()
