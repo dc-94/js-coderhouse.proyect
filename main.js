a3e498391e0b8bc94f4bd95889005b4837eb3c2f
Dan Coronel <dan.c94@outlook.com>
2021-11-29 15:14:06 -0300

    desafio 9

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 let bodega = []
+//card productos
+let mainCards = document.getElementById("mainCards")
+//botones
+let addProduct = document.getElementById("addNew")
+let cancelAdd = document.querySelector(".mainProductForm-btn-cancel")
+//formulario
+let addProductForm = document.getElementById("addProductForm")
+//filtros
+let ordenABC = document.getElementById('abc')
+
 
 class Vino {
     constructor(nombre, cepa, bodega,img, cosecha, precio){
@@ -11,34 +21,81 @@ class Vino {
     }
 }
 
-bodega.push(new Vino("Carpe Diem", "Pinot Noir","La Espera","https://mly1ygebnr2t.i.optimole.com/ScIDir0.w5KU~57308/w:600/h:600/q:89/https://vinotecaleneas.com/wp-content/uploads/2020/11/La-Espera-Carpe-Diem-Pinot-Noir.jpg",2018, 780))
-bodega.push(new Vino("Mas Janeil Le Petit Pas", "Grenache Noir / Syrah / Carignan / Mourvedre", "Francois Lurton","https://images.vivino.com/thumbs/P6vDe0gcTJCVrTwGofbMzg_pb_600x600.png", 2016, 2120))
-bodega.push(new Vino("Monteagrelo", "Cabernet Franc", "Bressia","https://images.vivino.com/thumbs/4Ug9g_psSOejUXKDVSt4TA_pl_375x500.png", 2019, 2200))
-bodega.push(new Vino("Humberto Canale Estate", "Cabernet Franc", "Humberto Canale","https://vinotecaligier.com/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/b/e/be03905_2.jpg", 2019, 900))
-bodega.push(new Vino("Golden Reserve", "Malbec", "Trivento","http://cdn.shopify.com/s/files/1/0215/7051/9140/products/trivento-golden-reserve-malbec-2019-vino-trivento-438468.jpg?v=1629756944", 2019, 1250))
-bodega.push(new Vino("Corte Argentino", "Malbec", "Francois Lurton","https://www.espaciovino.com.ar/media/default/0001/53/thumb_52818_default_medium.jpeg", 2013, 3470))
-bodega.push(new Vino("Cono Sur Reserva", "Carmenere", "Cono Sur","https://vinotecaligier.com/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/v/i/vi10924_1.jpg", 2018, 1450))
-bodega.push(new Vino("Trivento Reserva", "Cabernet Sauvignon", "Trivento","https://www.espaciovino.com.ar/media/default/0001/58/thumb_57329_default_medium.jpeg", 2020, 490))
 
+bodega.push(new Vino("carpe Diem", "Pinot Noir","La Espera","https://mly1ygebnr2t.i.optimole.com/ScIDir0.w5KU~57308/w:600/h:600/q:89/https://vinotecaleneas.com/wp-content/uploads/2020/11/La-Espera-Carpe-Diem-Pinot-Noir.jpg",2018, 780))
+bodega.push(new Vino("mas Janeil Le Petit Pas", "Grenache Noir / Syrah / Carignan / Mourvedre", "Francois Lurton","https://images.vivino.com/thumbs/P6vDe0gcTJCVrTwGofbMzg_pb_600x600.png", 2016, 2120))
+bodega.push(new Vino("monteagrelo", "Cabernet Franc", "Bressia","https://images.vivino.com/thumbs/4Ug9g_psSOejUXKDVSt4TA_pl_375x500.png", 2019, 2200))
+bodega.push(new Vino("humberto Canale Estate", "Cabernet Franc", "Humberto Canale","https://vinotecaligier.com/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/b/e/be03905_2.jpg", 2019, 900))
+bodega.push(new Vino("golden reserve", "Malbec", "Trivento","http://cdn.shopify.com/s/files/1/0215/7051/9140/products/trivento-golden-reserve-malbec-2019-vino-trivento-438468.jpg?v=1629756944", 2019, 1250))
+bodega.push(new Vino("corte Argentino", "Malbec", "Francois Lurton","https://www.espaciovino.com.ar/media/default/0001/53/thumb_52818_default_medium.jpeg", 2013, 3470))
+bodega.push(new Vino("cono Sur Reserva", "Carmenere", "Cono Sur","https://vinotecaligier.com/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/v/i/vi10924_1.jpg", 2018, 1450))
+bodega.push(new Vino("trivento Reserva", "Cabernet Sauvignon", "Trivento","https://www.espaciovino.com.ar/media/default/0001/58/thumb_57329_default_medium.jpeg", 2020, 490))
 
-let mainCards = document.getElementById("mainCards")
-bodega.forEach(Vino => {
-    mainCards.innerHTML += `
-    <div class="mainCards_card">
-        <img class="mainCards_card_img" src="${Vino.img}" alt="">
-        <div class="mainCards_card_txt">
-            <h2 class="mainCards_card_txt_productName">${Vino.nombre}</h2>
-            <h4 class="mainCards_card_txt_desc">${Vino.bodega}</h4>
-            <h3 class="mainCards_card_txt_cepa">${Vino.cepa}
-            <br>- ${Vino.cosecha}
-            <p class="mainCards_card_txt_price">$${Vino.precio}</p>
+//mostrar productos cargados
+showCatalog = () => {
+    bodega.forEach((Vino, indice) => {
+        mainCards.innerHTML += `
+        <div class="mainCards_card" id="product_${indice+1}">
+            <img class="mainCards_card_img" src="${Vino.img}" alt="">
+            <div class="mainCards_card_txt">
+                <h2 class="mainCards_card_txt_productName">${Vino.nombre}</h2>
+                <h4 class="mainCards_card_txt_desc">${Vino.bodega}</h4>
+                <h3 class="mainCards_card_txt_cepa">${Vino.cepa}
+                <br>- ${Vino.cosecha}
+                <p class="mainCards_card_txt_price">$${Vino.precio}</p>
+            </div>
         </div>
-    </div>
-    `
-})
-//let addItem = document.getElementById("addItem")
+        `
+    })
+}
+    showCatalog()
 
+//agregar vino
+ addProductForm.addEventListener('submit',(e) =>{
+     e.preventDefault()
+     console.log(e.target)
+    let nombre = document.getElementById('nombre').value
+    let cepa = document.getElementById('cepa').value
+    let bodeg = document.getElementById('bodega').value
+    let img = document.getElementById('img').value
+    let cosecha = document.getElementById('cosecha').value
+    let precio = document.getElementById('precio').value
 
+    bodega.push(new Vino(nombre, cepa,bodeg,img,cosecha,precio))
+
+    addProductForm.reset()
+    mainCards.innerHTML=""
+    showCatalog()
+    hide()
+}) 
+
+//orden alfabetico
+ordenABC.onclick= () => {
+    ordenABC()
+}
+
+ordenABC = () => {
+    bodega.sort(function(a,b){
+        return a.nombre > b.nombre
+    })
+    mainCards.innerHTML=""
+    showCatalog()
+}
+addProduct.onclick = () =>{
+    show()
+}
+cancelAdd.onclick = () =>{
+    hide()
+}
+
+show = () =>{
+    let layout=document.querySelector(".mainProductForm")
+    layout.style.display="block"
+}
+hide = () =>{
+    let layout=document.querySelector(".mainProductForm")
+    layout.style.display="none"
+}
 // guardar JSON
 /*function localSave(){
     let aJson = JSON.stringify(bodega)
@@ -51,86 +108,4 @@ localSave()*/
 //     const bodega = JSON.parse(deJson)
 //}
 //
-// //pregunta si agregar mas o no
-function masVino(){
-    let morewine = prompt("desea agregar mas vino? SI/NO").toLowerCase()
-    if(morewine == "si"){
-        agregarVino()
-        masVino()
-    }else if(morewine == "no"){
-        mainQuestion()
-    }else{
-     alert("Era SI o NO lo que tenias que responder!")   
-    }
-}
 //
-//agregar vino
-function agregarVino(){
-    bodega.push(new Vino(prompt("Ingrese el nambre del vino"), prompt("Ingrese la cepa"), 
-    prompt("Ingrese la bodega"), 
-    parseInt(prompt("Ingrese el año de cosecha")), 
-    parseInt(prompt("Ingrese el precio"))
-        )
-    )
-//    localSave()
-    masVino()
-}
-// masVino()
-// //
-// //orden alfabetico
-// function ordenABC(){
-//     bodega.sort(function(a,b){
-//         return a.nombre > b.nombre
-//     })
-//     console.log(bodega)
-// }
-// //
-// //por precio > <
-// function searchPrecio(){
-//     let valor = parseInt(prompt("ingrese un precio (490 a 3580)"))
-//     let orden = prompt("desea buscar productos por debajo o sobre ese precio? ( < ó > )")
-//     if (orden == "<"){
-//         console.log(bodega.filter(vino => vino.precio <= valor))
-//     }else if(orden == ">"){
-//         console.log(bodega.filter(vino => vino.precio >= valor))
-//     }else{
-//         console.log("ingresaste un carácter no valido")
-//     }
-// }
-// //
-// //por cosecha
-// function searchCosecha(){
-//     let valor = parseInt(prompt("ingrese un año (2013 a 2020)"))
-//     let orden = prompt("desea buscar productos por debajo o sobre ese año de cosecha? ( < ó > )")
-//     if (orden == "<"){
-//         console.log(bodega.filter(vino => vino.cosecha <= valor))
-//     }else if(orden == ">"){
-//         console.log(bodega.filter(vino => vino.cosecha >= valor))
-//     }else{
-//         console.log("ingresaste un carácter no valido")
-//     }
-// }
-// //
-// //como buscar?
-// function searchMethod(){
-//     let searchQ = prompt(" desea ordenar los productos por orden alfabetico (A) ó buscar por precio (P) o cosecha (C)?").toUpperCase()
-//     if (searchQ == "A"){
-//         ordenABC()
-//     }else if(searchQ == "P"){
-//         searchPrecio()
-//     }else{
-//         searchCosecha()
-//     }
-// }
-// //que desea hacer?
-// function mainQuestion(){
-//     let firstQ = prompt("Quiere agregar, buscar un vino ó consultar el catálogo? (A/B/C)").toUpperCase()
-//     if (firstQ == "A"){
-//         agregarVino()
-//     }else if(firstQ == "B"){
-//         searchMethod()
-//     }else{
-//         alert("Podés consultar el catálogo en la consola y local Storage")
-//     }
-// }
-// mainQuestion()
